8ce6141326d0caf8365c2eb11837d30820a58ebd
Clement Buchanan <clement.buchanan@gmail.com>
2021-03-29 13:49:44 -0700

    it works. Events timestamping work

--- driver/driver.js
+++ driver/driver.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const events = require('./hub.js');
+const events = require('../hub.js');
 
 events.on('alert', getsPackage);
 
@@ -9,7 +9,7 @@ const date = new Date(timestamp * 1000);
 const hours = date.getHours();
 
 function getsPackage(payload) {
-  if (payload.notify === yes) {
+  if (payload.notify === true) {
     console.log('Your package is being picked up for delivery');
     events.emit('alert', `Your package was picked up on, ${date} at ${hours}`);
   }
