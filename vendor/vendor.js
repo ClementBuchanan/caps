d4e33b7c27f3d1216291b53438b06346f1114e4c
Clement Buchanan <clement.buchanan@gmail.com>
2021-03-31 15:17:36 -0700

    all work is completed except testing

--- vendor/vendor.js
+++ vendor/vendor.js
@@ -4,30 +4,34 @@ const events = require('../events/events.js');
 const faker = require('faker');
 const { fake } = require('faker');
 
+
 console.log('I am the vendor', fake);
 
 events.on('start', newSale)
 events.on('event', delivered)
 
 
+
 function newSale() {
   setInterval(() => {
+    let orderId = Math.floor(Math.random() * 1000);
     let payload = {
+      orderId: orderId,
       storeName: faker.company.companyName(),
       customerName: faker.name.findName(),
       customerAddress: faker.address.streetAddress(),
       customerZipcode: faker.address.zipCode(),
-
     }
 
     console.log(`New sale item is ready for pickup\n
+    ${payload.orderId}\n
     ${payload.storeName}\n
     ${payload.customerName}\n
     ${payload.customerAddress}\n
     ${payload.customerZipcode}\n`)
 
     events.emit('pickup', payload);
-  }, 2000);
+  }, 5000);
 }
 
 function delivered(payload) {
